c668f70e30d9f4bad5d51b24e4b81eb7c83b9e35
itssShiva <iitts.shiva@gmail.com>
2025-09-20 19:18:47 +0530

    Create recursiveReverseLinkedlist.cpp

---
+++ recursiveReverseLinkedlist.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+        ListNode* newhead = reverseList(head->next);
+        ListNode* front = head->next;
+        front->next = head;
+        head->next = nullptr;
+        return newhead;
+    }
+};
+
+// Helper function to print the linked list
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    // Create linked list: 1 -> 2 -> 3 -> 4 -> 5
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    cout << "Original list: ";
+    printList(head);
+
+    Solution sol;
+    ListNode* reversedHead = sol.reverseList(head);
+
+    cout << "Reversed list: ";
+    printList(reversedHead);
+
+    // Free memory (optional but good practice)
+    while (reversedHead != nullptr) {
+        ListNode* temp = reversedHead;
+        reversedHead = reversedHead->next;
+        delete temp;
+    }
+
+    return 0;
+}
