f1af645f835e69565d0f6f38d62222031ff3c4f4
itssShiva <iitts.shiva@gmail.com>
2025-10-07 00:02:27 +0530

    Create segregateEvenOdd.cpp

---
+++ segregateEvenOdd.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return head;
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenNode = head->next;
+        while (even != nullptr && even->next != nullptr) {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = evenNode;
+        return head;
+    }
+};
+
+ListNode* createList(const vector<int>& vals) {
+    if (vals.empty()) return nullptr;
+    ListNode* head = new ListNode(vals[0]);
+    ListNode* curr = head;
+    for (int i = 1; i < vals.size(); ++i) {
+        curr->next = new ListNode(vals[i]);
+        curr = curr->next;
+    }
+    return head;
+}
+
+void printList(ListNode* head) {
+    while (head) {
+        cout << head->val;
+        if (head->next) cout << " -> ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    vector<int> vals = {1, 2, 3, 4, 5};
+    ListNode* head = createList(vals);
+    cout << "Original List: ";
+    printList(head);
+    Solution sol;
+    head = sol.oddEvenList(head);
+    cout << "After Odd-Even Rearrangement: ";
+    printList(head);
+    return 0;
+}
